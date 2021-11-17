4f86b788ffed63cf8e17cb87c587a5bb0578a229
Sumesh <sumesh.majhi.5@gmail.com>
2021-11-17 11:32:31 +0530

    🚀 RELEASE: Add Strapi Backend App

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6667fd197b29630303522be4f034aca6'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3dcb6954-08b4-4d96-b576-2a29aa309c23'
+};
